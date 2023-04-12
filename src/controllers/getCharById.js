55ba5423652d4c1ca9ec430ba5ad27e2773baf85
EmilceAide <emilceaide.t@gmail.com>
2023-04-12 12:09:47 -0500

    comments were removed

--- src/controllers/getCharById.js
+++ src/controllers/getCharById.js
@@ -14,28 +14,6 @@ const getCharById = (req, res) =>{
         res.status(500).json({error: err.message})
     })
 
-
-// axios.get(`https://rickandmortyapi.com/api/character/${id}`)
-// .then(result => result.data)
-// .then(data => {
-//     let character = {
-//         id: data.id,
-//         name: data.name,
-//         image: data.image, 
-//         gender: data.gender, 
-//         species: data.species
-//     }
-//     res
-//     .writeHead(200, {"Content-type": "application/json"})
-//     .end(JSON.stringify(character))
-
-// })
-// .catch(err => 
-//     res.writeHead(500, {"Content-type": "text/plain"})
-//     .end(`El personaje con id: ${id} no fue encontrado`)
-    
-//     )
-
 }
 
 module.exports = getCharById; 
